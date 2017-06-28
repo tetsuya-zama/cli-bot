10bf8540c154ee16ac9e32f2d3ea211a0ae762e0
Kunugi-NST <sabamiso48@gmail.com>
2017-06-28 17:52:27 +0900

    功刀がチケット3を反映しました

--- app3.js
+++ app3.js
@@ -76,6 +76,10 @@ function createSpeaker(tokenizer,debug){
     .concat(
       tokens.filter(function(token){ return token.surface_form === "真田" || token.surface_form === "幸村"})
       .map(function(token){ return "天下一の兵！（By上杉景勝）";})
+    )
+	.concat(
+      tokens.filter(function(token){ return token.surface_form === "功刀"})
+      .map(function(token){ return "ちがうよ、藤森だよ。";})
     );
 
     if(debug){
