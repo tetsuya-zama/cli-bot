3dac1b78109bdfb3443d3e491b420e045b08ae05
Sameshiman <yukari_sameshima@nri-st.co.jp>
2017-06-28 17:52:49 +0900

    75-79
    小栗旬追加

--- app2.js
+++ app2.js
@@ -72,6 +72,11 @@ function createSpeaker(tokenizer,debug){
       tokens.filter(function(token){ return token.surface_form === "？" || token.surface_form === "?"})
       .map(function(token){ return "うん。";})
     )
+    //Add sameshima
+    .concat(
+      tokens.filter(function(token){ return token.surface_form === "小栗旬" || token.surface_form === "小栗旬"})
+      .map(function(token){ return "かっこいいよね";})
+    )
     //Add Taiko 2017/6/28 15:47
     .concat(
       tokens.filter(function(token){ return token.surface_form === "真田" || token.surface_form === "幸村"})
